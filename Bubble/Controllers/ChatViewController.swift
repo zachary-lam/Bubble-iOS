fcb33dc7418761c37835ea3119858bd7314f001c
Zachary Lam <z74m+github@proton.me>
2026-06-22 16:48:55 -0500

    feat(storyboard): linked ib actions and outlets to view controllers

--- Bubble/Controllers/ChatViewController.swift
+++ Bubble/Controllers/ChatViewController.swift
@@ -9,21 +9,9 @@ import UIKit
 
 class ChatViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var messageTextField: UITextField!
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func sendPressed(_ sender: UIButton) {
     }
-    */
-
 }
--- Bubble/Controllers/LoginViewController.swift
+++ Bubble/Controllers/LoginViewController.swift
@@ -9,21 +9,9 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func loginPressed(_ sender: UIButton) {
     }
-    */
-
 }
