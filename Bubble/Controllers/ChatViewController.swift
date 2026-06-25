a36d5ae38a43c55b58e2fe0e7562fa5e9bddaf4b
Zachary Lam <z74m+github@proton.me>
2026-06-25 13:40:10 -0500

    feat(backend): signout method implementation within chat view controller
    linked up the UIBarButtonItem so that when pressed authenticated users are logged out and brought back to the welcome view controller

--- Bubble/Controllers/ChatViewController.swift
+++ Bubble/Controllers/ChatViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ChatViewController: UIViewController {
 
@@ -16,5 +17,15 @@ class ChatViewController: UIViewController {
     }
     
     @IBAction func logoutPressed(_ sender: UIBarButtonItem) {
+        do {
+          try Auth.auth().signOut()
+            // Source - https://stackoverflow.com/a/34325813
+            // Posted by Rene Ramirez
+            // Retrieved 2026-06-25, License - CC BY-SA 3.0
+            self.navigationController?.popToRootViewController(animated: true)
+
+        } catch let signOutError as NSError {
+          print("Error signing out: %@", signOutError)
+        }
     }
 }
