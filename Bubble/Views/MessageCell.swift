4e09e8def526e8d846f517992f523a40cc4b0e52
Zachary Lam <z74m+github@proton.me>
2026-06-29 11:34:03 -0500

    feat(ui): custom message cell component

---
+++ Bubble/Views/MessageCell.swift
@@ -0,0 +1,23 @@
+//
+//  MessageCell.swift
+//  Bubble
+//
+//  Created by Zachary Lam on 2026-06-29.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
