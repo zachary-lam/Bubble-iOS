de220ffe0ae4a0c68a493ee4f3476553c8b40985
Zachary Lam <z74m+github@proton.me>
2026-06-29 11:39:01 -0500

    chore(ui): link outlets from component to controller

--- Bubble/Views/MessageCell.swift
+++ Bubble/Views/MessageCell.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class MessageCell: UITableViewCell {
 
+    @IBOutlet weak var messageBubble: UIView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var rightImageView: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
